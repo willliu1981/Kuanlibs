45432aeeceff77f688b53975febaa9580589dfb4
willliu1981 <h111107231675>
2023-09-19 02:03:35 +0800

    InitComponentActivity 介面 新增completeActivity default 方法

--- kuanandroidlibs/src/main/java/idv/kuan/kuanandroidlibs/components/InitComponentActivity.java
+++ kuanandroidlibs/src/main/java/idv/kuan/kuanandroidlibs/components/InitComponentActivity.java
@@ -1,8 +1,28 @@
 package idv.kuan.kuanandroidlibs.components;
 
+import android.content.Context;
+import android.content.Intent;
+import android.app.Activity;
+
 public interface InitComponentActivity {
 
     public void init();
+
     public void initComponents();
+
+    default void completeActivity(Context context) {
+        completeActivity(context, null);
+
+    }
+
+    default void completeActivity(Context context, Intent intent) {
+        if (intent == null) {
+            ((Activity) context).setResult(Activity.RESULT_OK);
+        } else {
+            ((Activity) context).setResult(Activity.RESULT_OK, intent);
+        }
+
+        ((Activity) context).finish();
+    }
 
 }
