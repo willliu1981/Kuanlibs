57439fa9d4cc08c648cbd494f60e805346da5696
willliu1981 <h111107231675>
2023-11-02 20:33:42 +0800

    MetadataEneityUtil 的serializeMetadata 實作

--- common/src/main/java/idv/kuan/libs/databases/models/MetadataEntityUtil.java
+++ common/src/main/java/idv/kuan/libs/databases/models/MetadataEntityUtil.java
@@ -1,8 +1,10 @@
 package idv.kuan.libs.databases.models;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class MetadataEntityUtil {
     MetadataEntity.Metadata data;
@@ -12,11 +14,23 @@ public class MetadataEntityUtil {
 
     }
 
+    public static byte[] serializeMetadata(MetadataEntity.Metadata metadata) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            oos.writeObject(metadata);
+            oos.flush();
+            return bos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException("Serialization of metadata failed", e);
+        }
+    }
+
     public static MetadataBuilder metadataBuilder() {
 
         return new MetadataBuilder();
 
     }
+
 
     public static class MetadataBuilder {
 
@@ -28,12 +42,11 @@ public class MetadataEntityUtil {
         }
 
 
-        public MetadataEntity.Metadata build() {
+        public MetadataEntity.Metadata buildMetadata() {
 
             if (data != null) {
                 ByteArrayInputStream bis = new ByteArrayInputStream(data);
-                try {
-                    ObjectInputStream ois = new ObjectInputStream(bis);
+                try (ObjectInputStream ois = new ObjectInputStream(bis)) {
                     return (MetadataEntity.Metadata) ois.readObject();
                 } catch (IOException | ClassNotFoundException e) {
                     //throw new RuntimeException("Failed to deserialize metadata", e);
@@ -44,8 +57,9 @@ public class MetadataEntityUtil {
 
             return null;
         }
-    }
 
+
+    }
 
 
 }
