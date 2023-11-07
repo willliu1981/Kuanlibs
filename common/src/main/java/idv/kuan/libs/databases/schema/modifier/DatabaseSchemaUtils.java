b1c2fb50b839016d6405ce9f5aba2830cea40ba9
willliu1981 <h111107231675>
2023-11-07 23:08:31 +0800

    修改DatabaseSchemaUtils 中 List<SchemaModifier>的結構,不使用泛型

--- common/src/main/java/idv/kuan/libs/databases/schema/modifier/DatabaseSchemaUtils.java
+++ common/src/main/java/idv/kuan/libs/databases/schema/modifier/DatabaseSchemaUtils.java
@@ -17,21 +17,21 @@ public class DatabaseSchemaUtils {
          * @param modifierBuilder
          * @param modifiers
          */
-        public <T extends SchemaModifier> void execute(SchemaModifierHandler.SchemaModifierBuilder modifierBuilder, List<T> modifiers);
+        public   void execute(SchemaModifierHandler.SchemaModifierBuilder modifierBuilder, List<SchemaModifier> modifiers);
     }
 
 
     /**
      * 重要:使用該方法前需使用DBFactoryConfiguration註冊database 相關資訊,
-     * executor 只有一個execute方法,
+     * UpdateSchemaExecutor 只有一個execute方法,
      * 並且提供有SchemaModifierHandler.SchemaModifierBuilder工具來創造SchemaModifier實例
      *
      * @param executor
      */
-    public static <T extends SchemaModifier> void checkAndUpdateSchema(VersionHelper helper, UpdateSchemaExecutor executor) {
+    public static   void checkAndUpdateSchema(VersionHelper helper, UpdateSchemaExecutor executor) {
         SchemaModifierHandler handler = new SchemaModifierHandler(DBFactoryConfiguration.getFactory().getConnection(), helper.getVersionCode());
-        List<T> modifiers = new ArrayList<>();
-        executor.execute(handler.getSchemaModifierBuilder(), modifiers);
+        List<SchemaModifier> modifiers = new ArrayList<>();
+        executor.execute(handler.getSchemaModifierBuilder(), (List<SchemaModifier>) modifiers);
         modifiers.forEach(m -> handler.addSchemaModifier(m));
         handler.execute();
     }
