196eeaaccde71a26db1ead95ec39af4dca7477f9
willliu1981 <h111107231675>
2023-12-27 07:12:38 +0800

    修改 Observer 和Subject

--- common/src/main/java/idv/kuan/libs/interfaces/observers/Observer.java
+++ common/src/main/java/idv/kuan/libs/interfaces/observers/Observer.java
@@ -1,6 +1,6 @@
 package idv.kuan.libs.interfaces.observers;
 
-public interface Observer<T> {
+public interface Observer<T extends Observer> {
 
     void update(T data);
 
--- common/src/main/java/idv/kuan/libs/interfaces/observers/Subject.java
+++ common/src/main/java/idv/kuan/libs/interfaces/observers/Subject.java
@@ -1,6 +1,6 @@
 package idv.kuan.libs.interfaces.observers;
 
-public interface Subject<T> {
+public interface Subject<T extends Observer> {
     void registerObserver(Observer<T> observer);
 
     void removeObserver(Observer<T> observer);
